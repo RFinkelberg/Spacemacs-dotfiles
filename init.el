e2f5a130c5d635a95718f07ef0f47b8df0db949c
Roy Finkelberg <rfinkelberg3@gatech.edu>
2016-12-23 21:27:37 -0500

    test

--- init.el
+++ init.el
@@ -150,7 +150,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("DejaVu LGC Sans Mono"
-                               :size 15 ;; was Inconsolata size 16
+                               :size 15  ;; was Inconsolata size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
